ec276488eeaac2990613adb2805c17fb1f6ed738
Devon Bernhagen <devon.bernhagen@gmail.com>
2022-11-08 13:04:52 -0800

    Javascrip file

---
+++ js-file.js
@@ -0,0 +1,8 @@
+const rock =document.querySelector('#Rock');
+rock.addEventListener('click', rockChoice); 
+
+const paper =document.querySelector('#Paper');
+paper.addEventListener('click', paperChoice); 
+
+const scissors =document.querySelector('#Scissors');
+scissors.addEventListener('click', scissorsChoice); 
